e392c11151c11b118c02656f13d3bcb27c66ca45
Aakash Chahal <aakash.chahal62@gmail.com>
2021-04-09 02:23:35 +0530

    UPDATE

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 "use strict";
 // will add some JavaScript later
+const contact = document.getElementById("contact");
+const send = document.getElementById("send-msg");
+// console.log(myName);
+contact.addEventListener("click", function () {
+    send.addEventListener("click", function () {});
+});
+// will work on this later once done with backend
